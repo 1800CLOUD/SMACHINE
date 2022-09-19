b9dd78a347557bbdb6e67982798fd4317dd1685a
Juanh Pablo Arcos <jparcos@agofer.com.co>
2022-09-19 16:28:20 +0000

    QA_SMACHINE [REF] base: refactor manifest

--- base_smachine/__manifest__.py
+++ base_smachine/__manifest__.py
@@ -10,9 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "1-800sap",
-    'contributors': ["Bernardo Lara"],
-    'website': "https://1-800sap.com",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
