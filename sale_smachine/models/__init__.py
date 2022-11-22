9dc26a23b9eb71343bf79be1f3fc39b696c060b4
bitsfernandofernandez <nelson.fernandez@bitsamericas.com>
2022-11-22 12:04:29 -0500

    [COMMENT] Comment line res_company and res_config_settings

--- sale_smachine/models/__init__.py
+++ sale_smachine/models/__init__.py
@@ -4,5 +4,5 @@ from . import sale_order
 from . import account_fiscal_position
 from . import account_payment_mode
 from . import mrp_bom
-from . import res_company
-from . import res_config_settings
+#from . import res_company
+#from . import res_config_settings
