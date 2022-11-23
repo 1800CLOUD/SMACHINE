fe4931da40496e35330015bd1ed39e96067bd5f5
blara <bdlgrw@gmail.com>
2022-11-23 11:30:25 -0500

    [IMP] #958: Campo tipo cliente en el tercero

--- sale_smachine/__manifest__.py
+++ sale_smachine/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "https://1-800cloud.com/",
     'license': 'OPL-1',
     'category': 'Inventory/Sale',
-    'version': '15.0.0.0.1',
+    'version': '15.0.0.0.2',
     'depends': [
         'sale_baseline'
     ],
