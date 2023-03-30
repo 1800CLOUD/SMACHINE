80494dfba55abf7f39fbfef6392a1498d45a2e6b
Juan Arcos <juanparmer@gmail.com>
2023-03-29 22:27:22 -0500

    v15 [REF] helpdesk eliminate inherit

--- helpdesk_smachine/models/damage_type_sm.py
+++ helpdesk_smachine/models/damage_type_sm.py
@@ -3,10 +3,6 @@
 from odoo import fields, models, _
 
 
-class BankPaymentLine(models.Model):
-    _name = 'bank.payment.line'
-    _inherit = 'bank.payment.line'
-
 class DamageTypeSm(models.Model):
     _name = 'damage.type.sm'
     _description = 'Damage type for helpdesk'
