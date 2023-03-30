17a38510780fb9937e17bb6c4c8cddecd0bb94fb
Juan Arcos <juanparmer@gmail.com>
2023-03-29 22:18:08 -0500

    v15 [REF] helpdesk: add module bank.payment.line

--- helpdesk_smachine/models/damage_type_sm.py
+++ helpdesk_smachine/models/damage_type_sm.py
@@ -5,6 +5,7 @@ from odoo import fields, models, _
 
 class BankPaymentLine(models.Model):
     _name = 'bank.payment.line'
+    _inherit = 'bank.payment.line'
 
 class DamageTypeSm(models.Model):
     _name = 'damage.type.sm'
