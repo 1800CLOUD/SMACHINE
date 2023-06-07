dae1aa992d547c9baa685ff6a6742e6391f9b8dd
dfcastano <diegoferledesma@gmail.com>
2023-06-07 17:23:58 -0500

    [ADD] TK-1861 report orders pos and add vendedors by default in invoice

--- report_invoice/__manifest__.py
+++ report_invoice/__manifest__.py
@@ -11,7 +11,7 @@
     'website': 'https://1-800cloud.com',
     'category': 'sales/sale',
     'license': 'LGPL-3',
-    'version': '15.0.0.0.7',
+    'version': '15.0.0.0.8',
     'depends': [
         'base_setup',
         'account_baseline',
