6e9a5446ceece5f57e25f22bd6f53abef1e9c53e
blara <bdlgrw@gmail.com>
2023-12-07 12:47:19 -0500

    [IMP] #2311-00094, calculo de cumplimiento, presupuesto ventas

--- seller_budget/models/res_company.py
+++ seller_budget/models/res_company.py
@@ -8,7 +8,7 @@ class ResCompany(models.Model):
     percent_budget = fields.Selection(
         [('theoritical', 'Importe Teórico'),
          ('planned', 'Importe Previsto')],
-        'Cálculo de cumplimiento'
+        'Cálculo de cumplimiento',
         default='planned',
         help='Define si el logro en las líneas de presupuesto se debe calcular'
         ' respecto al importe teórico o el importe previsto')
